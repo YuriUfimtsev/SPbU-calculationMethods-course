b7f2aa5858d75b4a3a5b14a21d66319716d37bdc
ArtyomKopan <artem.kopan.2003@mail.ru>
2023-09-10 21:53:26 +0300

    Change of solution

--- task_1.py
+++ task_1.py
@@ -13,15 +13,6 @@ def f2(x):  # f2(x) := f''(x)
     return -20 * sin(2 * x) - 0.5 * (1 - x) ** (-3 / 2)
 
 
-print('Введите границы промежутка [A, B]:')
-A = float(input('A = '))
-B = float(input('B = '))
-
-N = int(input('Введите количество отрезков, на которых нужно искать корни: '))
-eps = float(input('Введите точность: '))
-
-
-# Отделение корней
 def get_roots_segments(A, B, N):
     h = (B - A) / N
     control_points = [A + i * h for i in range(0, N + 1)]
@@ -33,14 +24,8 @@ def get_roots_segments(A, B, N):
     return segments
 
 
-# поиск корней
-segments = get_roots_segments(A, B, N)
-bisect_roots, secant_roots = [], []
-for segment in segments:
+def bisect(segment):
     a, b = segment[0], segment[1]
-
-    # метод бисекции
-    # print('Метод бисекции: ')
     while b - a >= 2 * eps:
         c = a + (b - a) / 2
         if f(a) * f(c) < 0:
@@ -48,25 +33,40 @@ for segment in segments:
         else:
             a, b = c, b
     x_ = (a + b) / 2
-    bisect_roots.append(x_)
+
+    print('Метод бисекции: ')
+    print('x = ' + str(x_) + '; f(x) = ' + str(f(x_)))
+
+
+def secant(segment):
+    a, b = segment[0], segment[1]
+    x = [a + (b - a) / 2, a + (b - a) / 2]
+    while abs(x[-1] - x[-2]) >= eps:
+        x.append(x[-1] - f(x[-1]) / (f(x[-1]) - f(x[-2])))
+    print('Метод секущих: ')
+    print('x = ' + str(x[-1]) + '; f(x) = ' + str(f(x[-1])) + '; Δ = ' + str(abs(x[-1] - x[-2])))
+
+
+print('Введите границы промежутка [A, B]:')
+A = float(input('A = '))
+B = float(input('B = '))
+
+N = int(input('Введите количество отрезков, на которых нужно искать корни: '))
+eps = float(input('Введите точность: '))
+
+# Отделение корней
+
+
+# поиск корней
+segments = get_roots_segments(A, B, N)
+bisect_roots, secant_roots = [], []
+for segment in segments:
+    # метод бисекции
+    bisect(segment)
 
     # метод Ньютона
 
     # модифицированный метод Ньютона
 
     # метод секущих
-    # print('Метод секущих: ')
-    a, b = segment[0], segment[1]
-    x = [a + (b - a) / 2, a + (b - a) / 2]
-    while abs(x[-1] - x[-2]) >= eps:
-        x.append(x[-1] - f(x[-1]) / (f(x[-1]) - f(x[-2])))
-    # print(x[-1])
-    secant_roots.append(x[-1])
-
-print('Метод бисекции: ')
-for i in bisect_roots:
-    print(i)
-print()
-print('Метод секущих: ')
-for i in secant_roots:
-    print(i)
+    secant(segment)
