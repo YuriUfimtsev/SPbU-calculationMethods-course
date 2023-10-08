7e181477f1917b8346096b49e1a834b93051f00d
YuriUfimtsev <ufimtsev3007@gmail.com>
2023-10-08 18:32:22 +0300

    wip

--- task_3/numerical_differentiation.py
+++ task_3/numerical_differentiation.py
@@ -5,4 +5,14 @@ def f(x):
     return exp(7.5 * x)
 
 
-def print_function_derivatives_table(a, m, h, default_function_table):
+
+def form_function_derivatives_table(default_function_table):
+    result_table = [[0.0] * 8 for _ in range(len(default_function_table))]
+    _copy_default_function_table_values(default_function_table, result_table)
+
+
+
+def _copy_default_function_table_values(default_function_table, result_derivatives_table):
+    for i in range(len(default_function_table)):
+        for j in range(len(default_function_table[0])):
+            result_derivatives_table[i][j] = default_function_table[i][j]
