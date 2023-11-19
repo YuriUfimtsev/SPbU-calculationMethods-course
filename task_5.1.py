f20c242248a87ad9df19330cad5e3fae87499d1b
ArtyomKopan <64657781+ArtyomKopan@users.noreply.github.com>
2023-11-20 02:46:11 +0300

    Update task_5.1.py

--- task_5.1.py
+++ task_5.1.py
@@ -1,18 +1,36 @@
+from scipy import integrate, linalg
+from math import sin
 
 
-print('Задача приближённого вычисления определённого интеграла с помощью квадратурных формул Гаусса')
+def f(x_):
+    return sin(x_)
+
+
+def p(x_):
+    return 1 / (x_ + 0.1)
+
+
+def moment(n, a, b):
+    return integrate.quad(lambda x_: p(x_) * (x_ ** n), a, b)[0]
+
+
+print('Задача приближённого вычисления определённого интеграла с помощью ИКФ')
 print('Вариант 9')
 
-print('Введите количество узлов интерполяции:')
-N = int(input('N = '))
-print('Введите узлы интерполяции через пробел:')
-x = [int(i) for i in input().split()]
-for i in range(N):
-    for j in range(i + 1, N):
-        if i != j and x[i] == x[j]:
-            print('Вы ввели неправильный набор узлов интерполяции: два узла совпадают')
-            break
-else:
-    # находим моменты мю
+while True:
+    print('Введите отрезок интегрирования:')
+    A = float(input('A = '))
+    B = float(input('B = '))
+    print('Введите количество узлов интерполяции:')
+    N = int(input('N = '))
+    h = (B - A) / N
+    x = [A + k * h for k in range(N)]
+    C = [[x[j] ** i for j in range(N)] for i in range(N)]
+    d = [round(moment(i, A, B), 3) for i in range(N)]
+    a = linalg.solve(C, d)
+    J_approximate = sum([a[k] * f(x[k]) for k in range(N)])
+    J = integrate.quad(lambda x_: f(x_) * p(x_), A, B)[0]
 
-    # составляем и решаем СЛАУ
+    print(f'Точное значение интеграла = {J}')
+    print(f'Приближённое значение интеграла = {J_approximate}')
+    print(f'Погрешность = {abs(J - J_approximate)}')
